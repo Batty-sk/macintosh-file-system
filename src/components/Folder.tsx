794c9b3d8e8192a70d7249e04f71817e27f71aec
Batty-sk <saorav.skumar@gmail.com>
2024-11-05 11:11:08 +0530

    saving changes

--- src/components/Folder.tsx
+++ src/components/Folder.tsx
@@ -1,51 +1,75 @@
-import React, { useState } from 'react'
-import { folder,close,correct } from '../assets'
-import { handleCreateGroup } from '../pinata'
+import React, { useState } from "react";
+import { folder, close, correct } from "../assets";
+import { handleCreateGroup } from "../pinata";
 
+type Props = {
+  newlyCreated: boolean;
+  name: string;
+  userId: string | undefined;
+  handleSwitchPath:(groupId:string)=>void;
+};
+const Folder = ({ newlyCreated, name, userId,handleSwitchPath }: Props) => {
+  const [isNewlyCreated, updateIsNewlyCreated] = useState(!newlyCreated);
+  const [folderName, updateFolderName] = useState("");
 
-type Props={
-    newlyCreated:boolean,
-    name:string,
-    userId: string|undefined,
-}
-const Folder = ({newlyCreated,name,userId}:Props) => {
-    
-    const [isNewlyCreated,updateIsNewlyCreated]=useState(!newlyCreated)
-    const [folderName,updateFolderName]=useState('')
-
-    const handleDoneEditing=async()=>{
-        //we call the pinaata api.
-        if(userId==undefined){
-            console.log('userId is undefined please first sign IN')
-            return 0
-        }
-        const res = await handleCreateGroup({name:userId+folderName})
-        console.log('response',res)
-        updateIsNewlyCreated(true)
+  const handleDoneEditing = async () => {
+    //we call the pinaata api.
+    if (userId == undefined) {
+      console.log("userId is undefined please first sign IN");
+      return 0;
     }
-    
-    const handleReset=()=>{
-        updateFolderName('')
-    }
+    const res = await handleCreateGroup({ name: userId + "|" + folderName });
+    console.log("response", res);
+    updateIsNewlyCreated(true);
+  };
+  console.log("name", name);
+  const handleReset = () => {
+    updateFolderName("");
+  };
   return (
-    <div className='flex items-center '>
-        <img src={folder} alt="folder" height={48} width={48} />
-    
-        {!isNewlyCreated?
-     <>
-        <input type="text" value={!newlyCreated?name:folderName} onChange={!newlyCreated?()=>{}:(e)=>{updateFolderName(e.target.value)}} />
-        <div className='flex'>
-                <img src={correct} height={20} width={20} alt="" onClick={()=>{
-                    handleDoneEditing();
-                }} className='cursor-pointer'/>
-                <img src={close} height={20} width={20} alt="" className='ml-2 cursor-pointer' onClick={()=>{
-                    handleReset();
-                }} />
-        </div>
+    <div className="flex items-center " onClick={()=>handleSwitchPath(userId+'|'+name)}>
+      <img src={folder} alt="folder" height={48} width={48} />
+      {!isNewlyCreated ? (
+        <>
+          <input
+            type="text"
+            value={!newlyCreated ? name : folderName}
+            onChange={
+              !newlyCreated
+                ? () => {}
+                : (e) => {
+                    updateFolderName(e.target.value);
+                  }
+            }
+          />
+          <div className="flex">
+            <img
+              src={correct}
+              height={20}
+              width={20}
+              alt=""
+              onClick={() => {
+                handleDoneEditing();
+              }}
+              className="cursor-pointer"
+            />
+            <img
+              src={close}
+              height={20}
+              width={20}
+              alt=""
+              className="ml-2 cursor-pointer"
+              onClick={() => {
+                handleReset();
+              }}
+            />
+          </div>
         </>
-        :<span className='pl-2'>{name?name:folderName}</span>}
+      ) : (
+        <span className="pl-2">{name ? name : folderName}</span>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Folder
+export default Folder;
--- src/components/FolderWindow.tsx
+++ src/components/FolderWindow.tsx
@@ -1,70 +1,109 @@
-import '../index.css'
-import { search } from '../assets'
-import Folder from './Folder'
-import { useEffect, useState } from 'react'
-import AddrBar from './AddrBar'
+import "../index.css";
+import { search } from "../assets";
+import Folder from "./Folder";
+import { useEffect, useState } from "react";
+import AddrBar from "./AddrBar";
 import { useUser } from "@clerk/clerk-react";
-import { handleRetrieveFiles } from '../pinata'
-import { handleRetrieveGroups,handleFilteredGroups } from '../pinata'
-import { FilteredGroupsProp } from '../pinata'
-
+import { handleRetrieveFiles, handleCreateGroup } from "../pinata";
+import { handleRetrieveGroups, handleFilteredGroups } from "../pinata";
+import { FilteredGroupsProp } from "../pinata";
 
 const FolderWindow = () => {
-  const { isLoaded, isSignedIn, user } = useUser(); 
-  const [path,updatePath] = useState("/home") 
-  const [folderDepth,updateFolderDepth]=useState(1)
+  const { isLoaded, isSignedIn, user } = useUser();
+  const [path, updatePath] = useState("/home");
+  const [folderAvailable, updateFolderAvailable] = useState<
+    FilteredGroupsProp[]
+  >([]);
+  const [folderDepth, updateFolderDepth] = useState(1);
 
-   // if useeffect runs on the clicking on the folder then we can increase the count
-   // this count will restrict the user from creating another folder couwe can display the dialog box modal  
-  useEffect(()=>{
-    const retrieveGroups= async ()=>{
-    if(user?.id==undefined)
-        return 0;
-    const groups = await handleRetrieveGroups()
-    if(groups){
-    const filteredGroups = handleFilteredGroups(groups.groups,user?.id);
-    console.log('filtered Groups!',filteredGroups)
-    }
+  // if useeffect runs on the clicking on the folder then we can increase the count
+  // this count will restrict the user from creating another folder couwe can display the dialog box modal
+  useEffect(() => {
+    const retrieveGroups = async () => {
+      if (user?.id == undefined) return 0;
+      const groups = await handleRetrieveGroups();
+      if (groups) {
+        const filteredGroups: FilteredGroupsProp[] = handleFilteredGroups(
+          groups.groups,
+          user?.id
+        );
+        updateFolderAvailable(filteredGroups)
+        console.log("filtered Groups!", filteredGroups);
       }
-      retrieveGroups()
-  },[])
-  
-  const handleUploadFileToPinata =(file:File)=>{
+    };
+    retrieveGroups();
+  }, []);
+
+  const handleUploadFileToPinata = (file: File) => {};
+  const [foldersCount, updateFoldersCount] = useState(0);
+  const [filesCount, updateFilesCount] = useState(0);
+
+  const handleSwitchPath =(groupId:string)=>{
+    //fetch the folders.
+    //update the folderDepth
+    //update the path
 
   }
-  const [foldersCount,updateFoldersCount] = useState(0)
-  const [filesCount,updateFilesCount] = useState(0)
 
   return (
-    <div className='w-full h-full '>
-        <div className='p-4 flex justify-between items-center'>
-            <div className='flex'>
-              <div>
-            <input className='title text-sm btn' type='file'  />
-            </div>
-            <button className='md:ml-4 btn' onClick={()=>updateFoldersCount(foldersCount+1)}>
-            <span className='title text-sm'>Create A Folder</span>
-            </button>
-            </div>
-            <div className='flex items-center'>
-            <img src={search} height={28} width={28} alt="" className='me-2' />
-            <input aria-label="Example text box" type="text" placeholder="Search" className='min-w-10'/>
-
-            </div>
-   
+    <div className="w-full h-full ">
+      <div className="p-4 flex justify-between items-center">
+        <div className="flex">
+          <div>
+            <input className="title text-sm btn" type="file" />
+          </div>
+          <button
+            className="md:ml-4 btn"
+            onClick={() => updateFoldersCount(foldersCount + 1)}
+          >
+            <span className="title text-sm">Create A Folder</span>
+          </button>
         </div>
-        <div className='address-bar'>
-      <AddrBar path={path}/>
+        <div className="flex items-center">
+          <img src={search} height={28} width={28} alt="" className="me-2" />
+          <input
+            aria-label="Example text box"
+            type="text"
+            placeholder="Search"
+            className="min-w-10"
+          />
         </div>
-
-        <div className='flex w-full h-full justify-center '>
-        <div className='pt-5 p-8 w-11/12 h-full folders overflow-auto mb-5'>
-
-      {!foldersCount && !filesCount?<h1 className='title text-2xl'>No Folders And Files.</h1>:new Array(foldersCount).fill(0).map((item,key)=>(<Folder key={key} newlyCreated={true} userId={user?.id} name='' />))}
+      </div>
+      <div className="address-bar">
+        <AddrBar path={path} />
+      </div>
+      <div className="flex w-full h-full justify-center ">
+        <div className="pt-5 p-8 w-11/12 h-full folders overflow-auto mb-5">
+          {folderAvailable.length
+            ? folderAvailable.map((item, index) => (
+                <Folder
+                  newlyCreated={false}
+                  key={index}
+                  userId={item.id}
+                  name={item.name.split('|')[1]}
+                  handleSwitchPath={handleSwitchPath}
+                />
+              ))
+            : null}
+          {!foldersCount || !folderAvailable.length && !filesCount ? (
+            <h1 className="title text-2xl">No Folders And Files.</h1>
+          ) : (
+            new Array(foldersCount)
+              .fill(0)
+              .map((item, key) => (
+                <Folder
+                  key={key}
+                  newlyCreated={true}
+                  userId={user?.id}
+                  name=""
+                  handleSwitchPath={handleSwitchPath}
+                />
+              ))
+          )}
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FolderWindow
+export default FolderWindow;
